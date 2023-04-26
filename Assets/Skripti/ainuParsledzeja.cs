6f15878b506e71d0337dc806e8e0646ba7c7eac9
Kraine <elina.kraine@sk.lvt.lv>
2023-04-26 12:14:05 +0300

    uielementi un sakums

---
+++ Assets/Skripti/ainuParsledzeja.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ainuParsledzeja : MonoBehaviour {
+    //Metode ļauj pārslēgties no UI ainas uz Sākuma ainu
+    public void uzSakums(){
+        SceneManager.LoadScene("Sakums", LoadSceneMode.Single);
+    }
+    //Metode, kura aizver programmu, ja tā ir izveidota kā .exe
+    public void Apturet(){
+        Application.Quit();
+    }
+    public void uzUIainu(){
+        SceneManager.LoadScene("UIelementi", LoadSceneMode.Single);
+    }
+}
